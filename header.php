e698b01c75cbdfd3bea58b4e6122064563382601
Unknown <joachimpw@gmail.com>
2017-03-27 12:22:57 +0200

    Header

---
+++ header.php
@@ -0,0 +1,21 @@
+
+
+
+<header>
+<video class="bv-video"></video>
+<nav class="navbar">
+<a href="index.php"><img src="img/logo.png" alt="jaja" class="logo"> </a>
+
+    <ul class="nav navbar-nav">
+      
+      <li><a href="index.php">Home </a></li>
+      <li><a href="#">Hotdogs </a></li>
+      <li><a href="#">Sodavand</a></li>
+      <li><a href="#">Kontakt</a></li>
+      <li><a href="#">Om os</a></li>
+      <li><a href="#"><img src="img/facebook-logo.png" alt="Facebook Logo" id="facebooklogo"></a></li>
+      <li><a href="#"><img src="img/instagram-logo.png" alt="Instragram logo" id="instagramlogo"></a></li>
+      <li><a href="#"><img src="img/google-plus-logo.png" alt="Google plus logo" id="googlepluslogo"></a></li>
+    </ul>    
+</nav>
+</header>
