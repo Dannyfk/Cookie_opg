8248dc52f95a3bd4d5521ed19580c492a2e413ae
Danny Kristensen <dfk2306@hotmail.com>
2017-03-27 13:59:54 +0200

    ny color picker

--- colorSite.php
+++ colorSite.php
@@ -9,19 +9,13 @@ include("includes/header.inc.php");
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
-      <h1 class="text-center">Color site</h1>
+      <h1 class="text-center">Hvad er din favorit farve</h1>
     </div>
   </div>
   <hr>
 </div>
-<div class="container">
-  <div class="row">
-    <div class="col-md-3" id="red" onclick="createCookie('color',id,7)"></div>
-      <div class="col-md-3" id="blue" onclick="createCookie('color',id,7)"></div>
-      <div class="col-md-3" id="green" onclick="createCookie('color',id,7)"></div>
-      <div class="col-md-3" id="yellow" onclick="createCookie('color',id,7)"></div>
-  </div>
-</div>
+
+<button onclick="something()">vælg favorit farve</button>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 <script src="js/jquery-1.11.3.min.js"></script>
@@ -31,7 +25,21 @@ include("includes/header.inc.php");
 
 <script src="js/functions.js"></script>
 
+<script src="js/jscolor.js"></script>
 
+Color: <input class="jscolor" id="jscolor" value="ab2567">
+
+
+
+
+<script>
+
+    function something(){
+        var yy = $("#jscolor").attr( "value" );
+        createCookie('color',yy,7)
+    }
+
+</script>
 
 
 
