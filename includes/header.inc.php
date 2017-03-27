0a8da898ed9b69a1f165da3457e45d0712ead109
Unknown <joachimpw@gmail.com>
2017-03-27 12:50:12 +0200

    CookieJS

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -3,13 +3,13 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">Cookie_opg</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+      <li><a href="#">Cookie 1</a></li>
+      <li><a href="#">Cookie 2 </a></li>
+      <li><a href="#">Cookie 3</a></li>
     </ul>
   </div>
 </nav>
