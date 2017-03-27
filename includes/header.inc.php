802944059844066d68bcbbdf684642f2a3f3f99d
Unknown <joachimpw@gmail.com>
2017-03-27 12:35:36 +0200

    Header

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -11,9 +11,6 @@
       <li><a href="#">Sodavand</a></li>
       <li><a href="#">Kontakt</a></li>
       <li><a href="#">Om os</a></li>
-      <li><a href="#"><img src="img/facebook-logo.png" alt="Facebook Logo" id="facebooklogo"></a></li>
-      <li><a href="#"><img src="img/instagram-logo.png" alt="Instragram logo" id="instagramlogo"></a></li>
-      <li><a href="#"><img src="img/google-plus-logo.png" alt="Google plus logo" id="googlepluslogo"></a></li>
-    </ul>    
+     </ul>
 </nav>
 </header>
